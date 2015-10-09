03399c769c8cc8afb7dd5d5e8cb3661e284ce245
Jens Maus <mail@jens-maus.de>
2015-10-09 11:11:31 +0200

    minor rewrite on startup script for better webui visibility.

--- generate_img.sh
+++ generate_img.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 mkdir -p tmp/script
 cp -a script/* tmp/script
+cp -a VERSION tmp/script/
 cp update_script tmp/
 cp scriptparser tmp/
 cd tmp
-tar -czvf ../scriptparser-1.1.tar.gz *
+tar -czvf ../scriptparser-$(cat ../VERSION).tar.gz *
 cd ..
 rm -rf tmp
