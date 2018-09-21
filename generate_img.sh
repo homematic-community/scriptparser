04b8bd7274f8617c41584f63b4e4a1060d23954e
Jens Maus <mail@jens-maus.de>
2018-09-21 15:25:29 +0200

    fixed install routines.

--- generate_img.sh
+++ generate_img.sh
@@ -5,6 +5,7 @@ cp -a VERSION tmp/script/
 cp update_script tmp/
 cp scriptparser tmp/
 cd tmp
-tar -czvf ../scriptparser-$(cat ../VERSION).tar.gz *
+find . -not -name '.DS_Store' -not -name '*.sha256' -type f -print0 | xargs -0 sha256sum >scriptparser-$(cat ../VERSION).sha256
+tar --owner=root --group=root --exclude=.DS_Store -czvf ../scriptparser-$(cat ../VERSION).tar.gz *
 cd ..
 rm -rf tmp
